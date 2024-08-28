f00a08e31d4af5bb6e80a4fc2e4131b164a738da
Jadzia Eleonore <bluakorvo@protonmail.com>
2024-08-28 10:20:31 -0600

    [INICIALIZA] el repositorio de git para lola la trailera

---
+++ Calculadora Lola/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculadora Lola
+//
+//  Created by Jadzia Gallegos on 28/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
