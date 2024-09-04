e190cd6706a588d8bf4ea574434f97b856eadf95
Jadzia Eleonore <bluakorvo@protonmail.com>
2024-09-04 10:37:43 -0600

    [AGREGA] la capacidad de reconocer el boton pulasdo

--- Calculadora Lola/ViewController.swift
+++ Calculadora Lola/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var boton_para_interactuar: UIButton!
+    
+    @IBOutlet weak var texto_a_cambiar: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    /// <#Description#>
+    /// - Parameter sender: <#sender description#>
+    @IBAction func que_hacer_al_pushar_boton(_ sender: UIButton) {
+        //texto_a_cambiar.text = (sender as UIButton).titleLabel.text
+        texto_a_cambiar.text = sender.titleLabel!.text ?? "Nosupe"
+        //boton_para_interactuar.titleLabel
+    }
+    
 
 }
 
