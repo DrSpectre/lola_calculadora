cd307bfb1127069133f1618d9a57e0b5313eaab8
Jadzia Eleonore <bluakorvo@protonmail.com>
2024-09-11 09:54:01 -0600

    [AGREGA] la capacidad de observar los hijos y de correcion de unos detallitos

--- Calculadora Lola/ViewController.swift
+++ Calculadora Lola/ViewController.swift
@@ -17,7 +17,12 @@ class ViewController: UIViewController {
     var estado_actual: estados_de_la_calculadora = estados_de_la_calculadora.seleccionar_numeros
     
     @IBOutlet weak var texto_a_cambiar: UILabel!
+    @IBOutlet weak var operacion_texto: UILabel!
+    @IBOutlet weak var segundo_termino_texto: UILabel!
+    
     @IBOutlet weak var boton_operacion: UIButton!
+    @IBOutlet weak var vista_stack: UIStackView!
+    
     
     var botones_interfaz: Dictionary<String, IUBotonCalculadora> = [:]
     var operacion_actual: String? = nil
@@ -34,14 +39,16 @@ class ViewController: UIViewController {
     /// - Parameter sender: <#sender description#>
     @IBAction func que_hacer_al_pushar_boton(_ sender: UIButton) {
         if(estado_actual == estados_de_la_calculadora.seleccionar_numeros){
-            // TODO: Arreglar glitch del text quitando el optional
-            let text_a_añadir = botones_interfaz[(sender.restorationIdentifier ?? boton_operacion.restorationIdentifier) ?? "boton"]?.numero
-            
-            texto_a_cambiar.text = "\(texto_a_cambiar.text ?? "")\(text_a_añadir!)"
+            //[X] TODO: Arreglar glitch del text quitando el optional
+            if let _mensajero_id = sender.restorationIdentifier{
+                let texto_cache = botones_interfaz[_mensajero_id]?.numero
+                texto_a_cambiar.text = "\(texto_a_cambiar.text ?? "")\(texto_cache!)"
+                
+            }
         }
         else if (estado_actual == estados_de_la_calculadora.escoger_operacion){
-            if let _mensajero: UIButton? = sender{
-                operacion_actual = botones_interfaz[_mensajero!.restorationIdentifier ?? "boton_0"]?.operacion
+            if let _mensajero_id = sender.restorationIdentifier{
+                operacion_actual = botones_interfaz[_mensajero_id]?.operacion
             }
             else {
                 operacion_actual = nil
@@ -59,6 +66,7 @@ class ViewController: UIViewController {
     
     
     func inicializar_calculadora() -> Void{
+        identificar_botones()
         crear_arreglo_botones()
     }
     
@@ -74,6 +82,28 @@ class ViewController: UIViewController {
         else if (estado_actual == estados_de_la_calculadora.seleccionar_numeros){
             
         }
+    }
+    
+    func identificar_botones(){
+        /*
+        for componente in self.view.subviews{
+            print(componente.restorationIdentifier)
+        }
+         */
+        
+        for pila_de_componentes in vista_stack.subviews{
+            for boton in pila_de_componentes.subviews{
+                print(boton.restorationIdentifier)
+            }
+        }
+        
+        
+        // self.view.subviews
+        /*for stack_con_vista in vista_stack.subviews {
+            for boton in stack_con_vista.subviews {
+                print(boton.restorationIdentifier)
+            }
+        }*/
     }
     
 
